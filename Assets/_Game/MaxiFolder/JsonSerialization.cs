52ccabbd09d7e38ab28a4b71430bb5a238c8b5e9
Durzdor <maxiperez024@hotmail.com>
2022-10-22 17:20:33 -0300

    Update JsonSerialization.cs
    Add deserialization

--- Assets/_Game/MaxiFolder/JsonSerialization.cs
+++ Assets/_Game/MaxiFolder/JsonSerialization.cs
@@ -21,6 +21,7 @@ namespace _Game.MaxiFolder
         private void Serialization()
         {
             PlayerObjs.Add(1,"test");
+            
             var data = new SerializationInfo
             {
                 pos = obj.transform.position,
@@ -35,12 +36,34 @@ namespace _Game.MaxiFolder
             Directory.CreateDirectory(Path.Combine(gameFolder, path));  
             var realPath = Path.Combine(gameFolder, path, filename + ".json");
             var json = JsonUtility.ToJson(data, true);
-        
+            
             // estas 3 lineas son lo mismo que el File.wrirtealltext lo dejo por si hay que separar el proceso en partes
             // StreamWriter file = File.CreateText(realPath);
             // file.Write(json);
             // file.Close();
             File.WriteAllText(realPath, json);
         }
+
+        [ContextMenu("LoadJson")]
+        private void Deserialization()
+        {
+            var gameFolder = Application.dataPath;
+            var realPath = Path.Combine(gameFolder, path, filename + ".json");
+            if (!File.Exists(realPath)) return;
+
+            // Las 3 lineas son la de abajo pero la dejo para separar si es necesario
+            // StreamReader file = File.OpenText(realPath);
+            // string json = file.ReadToEnd();
+            // file.Close();
+
+            var json = File.ReadAllText(realPath);
+            var data = JsonUtility.FromJson<SerializationInfo>(json);
+
+            print(data.hp);
+        }
+        
+        // Para poder utilizar monobehavior
+        // jsonutility.fromjsonoverwrite(json,monobehavior)
+       
     }
 }
